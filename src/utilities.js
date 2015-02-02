3cf7804525fed1cb2005eceee362557b104c8f33
Jeremy <jeremy@jeremydorn.com>
2015-02-02 07:19:28 -0800

    Fix for when an object has a property named "length". Fixes #328

--- src/utilities.js
+++ src/utilities.js
@@ -36,7 +36,7 @@ var $extend = function(destination) {
 var $each = function(obj,callback) {
   if(!obj) return;
   var i;
-  if(typeof obj.length !== 'undefined') {
+  if(typeof obj.length === 'number') {
     for(i=0; i<obj.length; i++) {
       if(callback(i,obj[i])===false) return;
     }
