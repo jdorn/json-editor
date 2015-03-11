cd7793359271797794077aaa0a6ce67b714cdc12
Jani Eväkallio <jani.evakallio@gmail.com>
2015-03-11 22:16:33 +0000

    Add CommonJS support
    If the environment defines a CommonJS module.exports interface, export JSONEditor to the environment. This adds support to build tools such as browserify.

--- src/outro.js
+++ src/outro.js
@@ -1,2 +1,8 @@
-  window.JSONEditor = JSONEditor;
+    /*global module*/
+    if (typeof module !== 'undefined' && module.exports) {
+      module.exports = JSONEditor;
+    }
+    if (typeof window !== 'undefined') {
+      window.JSONEditor = JSONEditor;
+    }
 })();
