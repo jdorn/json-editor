3158d921d739877108b689d4235cb8a382276bc8
Jeremy <jeremy@jeremydorn.com>
2015-05-08 06:49:18 -0700

    Use window.jQuery instead of '$'. Add select2 multiselect example.

--- src/editors/multiselect.js
+++ src/editors/multiselect.js
@@ -91,7 +91,7 @@ JSONEditor.defaults.editors.multiselect = JSONEditor.AbstractEditor.extend({
   },
   setupSelect2: function() {
     if(window.jQuery && window.jQuery.fn && window.jQuery.fn.select2) {
-        var options = $.extend({},JSONEditor.plugins.select2);
+        var options = window.jQuery.extend({},JSONEditor.plugins.select2);
         if(this.schema.options && this.schema.options.select2_options) options = $extend(options,this.schema.options.select2_options);
         this.select2 = window.jQuery(this.input).select2(options);
         var self = this;
