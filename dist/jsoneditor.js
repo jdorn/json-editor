57c4776b78e5dcc3e41e313fbfdf3e6ca4d5a051
Weihong Guan <weihong.guan@gmail.com>
2015-07-01 15:49:20 +0800

    set default height for summernote

--- dist/jsoneditor.js
+++ dist/jsoneditor.js
@@ -2121,13 +2121,14 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
 
       else if (this.options.wysiwyg === 'summernote' && this.input_type === 'html' && 
         window.jQuery && window.jQuery.fn && window.jQuery.fn.summernote) {
-        self.summernote_instance =window.jQuery(self.input).summernote({
+        self.summernote_instance =window.jQuery(self.input).summernote({          
+          height: 300,
           onBlur: function(e) {
             self.input.value = self.summernote_instance.code();
             self.value = self.input.value;
             self.is_dirty = true;
             self.onChange(true);
-          }
+          }          
         }); 
         self.summernote_instance.code(this.getValue());
       }
