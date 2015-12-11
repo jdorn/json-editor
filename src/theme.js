14606ed9c320f9f573e5cb9ea9bc7035cc141ba4
Bob Lannon <blannon@picwell.com>
2015-12-11 13:47:51 -0500

    added check for whether label is defined for checkbox

--- src/theme.js
+++ src/theme.js
@@ -170,7 +170,7 @@ JSONEditor.AbstractTheme = Class.extend({
     var el = document.createElement('div');
     el.className = 'form-control';
     if(label) el.appendChild(label);
-    if(input.type === 'checkbox') {
+    if(input.type === 'checkbox' && label) {
       label.insertBefore(input,label.firstChild);
     }
     else {
