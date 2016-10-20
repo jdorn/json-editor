e638f07481c358b832da59adbc28d2d989f9981c
Andrea Di Toro <andrea@androidsx.com>
2016-10-20 20:52:58 +0200

    new upload part 1

--- src/editors/upload.js
+++ src/editors/upload.js
@@ -2,7 +2,7 @@ JSONEditor.defaults.editors.upload = JSONEditor.AbstractEditor.extend({
   getNumColumns: function() {
     return 4;
   },
-  build: function() {    
+  build: function() {  
     var self = this;
     this.title = this.header = this.label = this.theme.getFormInputLabel(this.getTitle());
 
@@ -16,6 +16,8 @@ JSONEditor.defaults.editors.upload = JSONEditor.AbstractEditor.extend({
       if(!this.jsoneditor.options.upload) throw "Upload handler required for upload editor";
 
       // File uploader
+      var boxUploader =this.getButton('','upload','');
+      boxUploader.className= "box-upload";
       this.uploader = this.theme.getFormInputField('file');
       
       this.uploader.addEventListener('change',function(e) {
@@ -39,11 +41,22 @@ JSONEditor.defaults.editors.upload = JSONEditor.AbstractEditor.extend({
     if (!description) description = '';
 
     this.preview = this.theme.getFormInputDescription(description);
-    this.container.appendChild(this.preview);
-
-    this.control = this.theme.getFormControl(this.label, this.uploader||this.input, this.preview);
+    this.preview.style.display = 'inline-block';
+    boxUploader.appendChild(this.uploader);
+    this.control = this.theme.getFormControl(this.label, boxUploader,this.preview);
     this.container.appendChild(this.control);
+    this.addUploadPreview();
   },
+
+  postBuild: function() {
+    this.setupWatchListeners();
+    this.setValue(this.getDefault(), true);
+    this.updateHeaderText();
+    this.register();
+    this.onWatchedFieldChange();
+  },
+  
+
   refreshPreview: function() {
     if(this.last_preview === this.preview_value) return;
     this.last_preview = this.preview_value;
@@ -57,47 +70,26 @@ JSONEditor.defaults.editors.upload = JSONEditor.AbstractEditor.extend({
     var mime = this.preview_value.match(/^data:([^;,]+)[;,]/);
     if(mime) mime = mime[1];
     if(!mime) mime = 'unknown';
+    
 
     var file = this.uploader.files[0];
-
-    this.preview.innerHTML = '<strong>Type:</strong> '+mime+', <strong>Size:</strong> '+file.size+' bytes';
-    if(mime.substr(0,5)==="image") {
-      this.preview.innerHTML += '<br>';
-      var img = document.createElement('img');
-      img.style.maxWidth = '100%';
-      img.style.maxHeight = '100px';
-      img.src = this.preview_value;
-      this.preview.appendChild(img);
-    }
-
-    this.preview.innerHTML += '<br>';
-    var uploadButton = this.getButton('Upload', 'upload', 'Upload');
-    this.preview.appendChild(uploadButton);
-    uploadButton.addEventListener('click',function(event) {
-      event.preventDefault();
-
-      uploadButton.setAttribute("disabled", "disabled");
-      self.theme.removeInputError(self.uploader);
-
-      if (self.theme.getProgressBar) {
+    self.theme.removeInputError(self.uploader);
+    if (self.theme.getProgressBar) {
         self.progressBar = self.theme.getProgressBar();
         self.preview.appendChild(self.progressBar);
-      }
+    }
 
-      self.jsoneditor.options.upload(self.path, file, {
+    self.jsoneditor.options.upload(self.path, file, {
         success: function(url) {
           self.setValue(url);
-
           if(self.parent) self.parent.onChildEditorChange(self);
           else self.jsoneditor.onChange();
-
           if (self.progressBar) self.preview.removeChild(self.progressBar);
-          uploadButton.removeAttribute("disabled");
+
         },
         failure: function(error) {
           self.theme.addInputError(self.uploader, error);
           if (self.progressBar) self.preview.removeChild(self.progressBar);
-          uploadButton.removeAttribute("disabled");
         },
         updateProgress: function(progress) {
           if (self.progressBar) {
@@ -105,8 +97,49 @@ JSONEditor.defaults.editors.upload = JSONEditor.AbstractEditor.extend({
             else self.theme.updateProgressBarUnknown(self.progressBar);
           }
         }
-      });
     });
+  },
+
+  addUploadPreview : function(){
+    this.link_holder = this.theme.getLinksHolder();
+    this.link_holder.style.display =  "inline-block";
+    this.control.appendChild(this.link_holder);
+    if(this.schema.links) {
+      for(var i=0; i<this.schema.links.length; i++) {
+        var link = this.getImgLink(this.schema.links[i])
+        var preview = this.getImgPreview(this.schema.links[i])
+        this.link_holder.appendChild(link);
+        if(this.jsoneditor.options.initPopOver){
+          this.jsoneditor.options.initPopOver(link, preview);
+        }
+      }
+    }
+  },
+
+  getImgLink:function(){
+    var b = this.getButton('','preview','');
+    return b;
+  },
+
+  getImgPreview: function(data) {
+    var value = this.value;
+
+    var compiler = this.jsoneditor.compileTemplate(data.href,this.template_engine);
+    var url =  compiler({ 'self' :  value, 'url': value });
+    var pop = "<div><img style='max-width:350px' src='"+url+"'/></div>"
+    return pop;
+
+  },
+  refreshImgPreview : function(url){
+
+    if (this.link_holder){
+      for (var i = 0;  i < this.link_holder.children.length; i++){
+        if(this.jsoneditor.options.updatePopOver){
+          var pop = "<div><img style='max-width:350px' src='"+url+"'/></div>";
+          this.jsoneditor.options.updatePopOver(this.link_holder.children[i], pop);
+        }
+      }
+    }
   },
   enable: function() {
     if(this.uploader) this.uploader.disabled = false;
@@ -117,12 +150,15 @@ JSONEditor.defaults.editors.upload = JSONEditor.AbstractEditor.extend({
     this._super();
   },
   setValue: function(val) {
+    console.log("setValue" , val);
     if(this.value !== val) {
       this.value = val;
       this.input.value = this.value;
+      this.refreshImgPreview(this.value);
       this.onChange();
     }
   },
+
   destroy: function() {
     if(this.preview && this.preview.parentNode) this.preview.parentNode.removeChild(this.preview);
     if(this.title && this.title.parentNode) this.title.parentNode.removeChild(this.title);
