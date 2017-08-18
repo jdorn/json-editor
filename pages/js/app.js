3964627257e36daed85362df0a564ac3abe513f0
John Nolan <john.nolan@myunidays.com>
2017-08-18 16:36:18 +0100

    Oh lawd, so much stuff here

---
+++ pages/js/app.js
@@ -0,0 +1,25 @@
+var $_page;
+var $window;
+
+exports.ctor = function($page, path) {
+
+$_page = $page;
+$window = $(window);
+
+};
+
+exports.show = function() {
+
+};
+
+exports.hide = function() {
+
+};
+
+exports.onStudentChanged = function (student) {
+
+};
+
+exports.onViewportChanged = function (viewport, keyboardOpen) {
+
+};
